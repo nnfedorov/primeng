4c68846cb4ea34add3bc0e51fc303cb2e5ea5f6c
nnfedorov <nikola_fed1@mail.ru>
2023-05-26 11:44:32 +0300

    refs #24934 calendar component: target prop added to allow position calendar to the external element

--- src/app/components/calendar/calendar.ts
+++ src/app/components/calendar/calendar.ts
@@ -212,6 +212,7 @@ export class Calendar implements OnInit,OnDestroy,ControlValueAccessor {
     @Input() externalMonthsCreation?: boolean;
     @Input() onDateSelectFn?: (event: MouseEvent, dateMeta: DateMeta) => void;
     @Input() getDateClassFn?: (dateMeta: DateMeta) => string;
+    @Input() target?: HTMLElement;
 
     
     @Input() defaultDate: Date;
@@ -2083,9 +2084,9 @@ export class Calendar implements OnInit,OnDestroy,ControlValueAccessor {
         }
         else {
             if (this.appendTo)
-                DomHandler.absolutePosition(this.overlay, this.inputfieldViewChild.nativeElement);
+                DomHandler.absolutePosition(this.overlay, this.target || this.inputfieldViewChild.nativeElement);
             else
-                DomHandler.relativePosition(this.overlay, this.inputfieldViewChild.nativeElement);
+                DomHandler.relativePosition(this.overlay, this.target || this.inputfieldViewChild.nativeElement);
         }
     }
 
